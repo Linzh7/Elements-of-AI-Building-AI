152b7ad45791ffdd0e4f42cea5a90ab8a00fd740
Linzh <catmo1999@gmail.com>
2022-08-09 22:52:24 +0800

    add chap1 1.3

---
+++ 1.3.2 Probabilities.py
@@ -0,0 +1,15 @@
+import random
+
+
+def main():
+    prob = random.random()
+    if prob < 0.1:
+        favourite = "bats"
+    elif prob >= 0.1 and prob < 0.2:
+        favourite = "cats"
+    elif prob >= 0.2:
+        favourite = "dogs"
+    print("I love " + favourite)
+
+
+main()
